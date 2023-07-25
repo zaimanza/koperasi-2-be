00282a6ea4b78dcc916dc15934e345eb75c4a675
MANZA <36519974+zaimanza@users.noreply.github.com>
2023-07-25 12:42:06 +0800

    imit

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 3000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+const bodyParser = require("body-parser");
+var cors = require("cors");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    limit: "2000mb",
+    parameterLimit: 10000000000,
+  })
+);
+app.use(bodyParser.json({ limit: "2000mb" }));
+app.use(cors());
+
+app.use("/", require("./controllers"));
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
