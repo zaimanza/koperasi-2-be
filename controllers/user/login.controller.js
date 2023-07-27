9c8d6d4166957b3d38ded733c8197a2685e38e31
MANZA <36519974+zaimanza@users.noreply.github.com>
2023-07-27 13:46:49 +0800

    imp(): refresh2

--- controllers/user/login.controller.js
+++ controllers/user/login.controller.js
@@ -1,12 +1,32 @@
 const router = require("express").Router();
-
+const { User } = require("../../database/mongodb.database");
+// Example:
+// {
+//   "studentId": "123",
+//   "password": "12345"
+// }
 router.post("/login", async (req, res) => {
   try {
-    return res.status(200).json({
-      message: "fetched_people",
-    });
+    const userModel = await User();
+    const body = req.body;
+
+    const isStudentData = await userModel.findOne(body);
+
+    if (!isStudentData) {
+      return res.status(400).json({
+        acknowledged: false,
+        message: "Student does not exist.",
+      });
+    } else {
+      return res.status(400).json({
+        ...isStudentData,
+      });
+    }
   } catch (error) {
-    return res.status(400).json("Internal server problem. Try again later.");
+    return res.status(400).json({
+      acknowledged: false,
+      message: "Internal server problem. Try again later.",
+    });
   }
 });
 
