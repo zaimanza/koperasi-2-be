3048dd6a9bc5f1f0e34e14f0ca1ff35b90079d60
MANZA <36519974+zaimanza@users.noreply.github.com>
2023-07-27 13:13:21 +0800

    imp(): refetch

--- controllers/user/getById.controller.js
+++ controllers/user/getById.controller.js
@@ -1,29 +1,12 @@
 const router = require("express").Router();
-const { User } = require("../../database/mongodb.database");
-// Example: http://localhost:5000/user/getById?_id=64c1e9c71f8e51e418dc5ca8
+
 router.get("/getById", async (req, res) => {
   try {
-    const userModel = await User();
-    console.log(req.query);
-    const body = req.body;
-
-    const isStudentData = await userModel.findOne(body);
-
-    if (!isStudentData) {
-      return res.status(400).json({
-        acknowledged: false,
-        message: "Student does not exist.",
-      });
-    } else {
-      return res.status(400).json({
-        ...isStudentData,
-      });
-    }
-  } catch (error) {
-    return res.status(400).json({
-      acknowledged: false,
-      message: "Internal server problem. Try again later.",
+    return res.status(200).json({
+      message: "fetched_people",
     });
+  } catch (error) {
+    return res.status(400).json("Internal server problem. Try again later.");
   }
 });
 
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const { connectDB } = require("./database/mongodb.database");
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,7 +9,6 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 var cors = require("cors");
 
-// connectDB();
 app.use(
   bodyParser.urlencoded({
     extended: true,
