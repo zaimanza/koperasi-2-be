d07ebb7d1fcd94e50f12265c8e73e23cd1776af5
MANZA <36519974+zaimanza@users.noreply.github.com>
2023-07-29 14:20:46 +0800

    imp(): 200

--- controllers/parcel/get_by_id.controller.js
+++ controllers/parcel/get_by_id.controller.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { Parcel } = require("../../database/mongodb.database");
-// Example: http://localhost:5000/parcel/getById?parcelId=64c1e9c71f8e51e418dc5ca8
+// Example: http://localhost:5000/parcel/get_by_id?parcelId=64c1e9c71f8e51e418dc5ca8
 router.get("/get_by_id", async (req, res) => {
   try {
     const parcelModel = await Parcel();
@@ -14,7 +14,7 @@ router.get("/get_by_id", async (req, res) => {
         message: "Parcel does not exist.",
       });
     } else {
-      return res.status(400).json({
+      return res.status(200).json({
         ...isParcelData,
       });
     }
