ddc2326d9931d52bee6474e7638f3524ecf13ea3
MANZA <36519974+zaimanza@users.noreply.github.com>
2023-07-29 14:48:13 +0800

    update fix

--- controllers/parcel/out.controller.js
+++ controllers/parcel/out.controller.js
@@ -10,8 +10,10 @@ router.put("/out", async (req, res) => {
     const parcelModel = await Parcel();
 
     const body = req.body;
-    const isParcelData = await parcelModel.findOne(body);
 
+    const isParcelData = await parcelModel.findOne({
+      parcelId: body.parcelId,
+    });
     if (!isParcelData) {
       return res.status(400).json({
         acknowledged: false,
